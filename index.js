4fcf1036481adfb3a5d7a058887026142705c9ff
RubenCamus <Ruben-4-millet@hotmail.com>
2026-03-01 23:58:48 +0100

    Change keyboard logic and implement backspace and enter functionality.

--- index.js
+++ index.js
@@ -77,9 +77,9 @@ async function gameState(input) {
     var curRow = await getCurRow();
     var squaresArray = await getSquares(curRow);
     var focusSquare = squaresArray[curSquare];
-    focusSquare.value = input;
-    if (input == "Backspace") { curSquare--; return}
+    if (input == "Backspace" && curSquare < 0 ) { curSquare--; return}
     if (input == "Enter") { await sendInput(curRow); return}
+    focusSquare.value = input;
     if (curSquare < squaresArray.length - 1) curSquare++;
 }
 
